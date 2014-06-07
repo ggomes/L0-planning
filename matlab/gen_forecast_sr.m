060a46689e425d781e7f69f07f74e8b8fdb77468
Alex <akurzhan@gmail.com>
2014-06-06 23:09:06 -0700

    Generation of split ratios for the forecast year

--- matlab/gen_forecast_sr.m
+++ matlab/gen_forecast_sr.m
@@ -5,8 +5,7 @@ init_config;
 
 sz = range(2) - 1;
 
-or_id = xlsread(xlsx_file, 'On-Ramp_Demand', sprintf('g%d:g%d', range(1), range(2)));
-or_id = or_id';
+or_id = xlsread(xlsx_file, 'On-Ramp_Demand', sprintf('g%d:g%d', range(1), range(2)))';
 ORD = xlsread(xlsx_file, 'On-Ramp_Demand', sprintf('k%d:kl%d', range(1), range(2)));
 ORK = xlsread(xlsx_file, 'On-Ramp_Knobs', sprintf('k%d:kl%d', range(1), range(2)));
 ORD = ORD .* ORK;
@@ -14,8 +13,7 @@ has_or = find(max(ORD,[],2)>0);
 has_or = has_or';
 
 
-fr_id = xlsread(xlsx_file, 'Off-Ramp_Demand', sprintf('g%d:g%d', range(1), range(2)));
-fr_id = fr_id';
+fr_id = xlsread(xlsx_file, 'Off-Ramp_Demand', sprintf('g%d:g%d', range(1), range(2)))';
 FRD = xlsread(xlsx_file, 'Off-Ramp_Demand', sprintf('k%d:kl%d', range(1), range(2)));
 FRK = xlsread(xlsx_file, 'Off-Ramp_Knobs', sprintf('k%d:kl%d', range(1), range(2)));
 FRD = FRD .* FRK;
@@ -23,6 +21,8 @@ has_fr = find(max(FRD,[],2)>0);
 has_fr = has_fr';
 
 
+gp_id = xlsread(xlsx_file, 'On-Ramp_Demand', sprintf('a%d:a%d', range(1), range(2)))';
+hov_id = xlsread(xlsx_file, 'On-Ramp_Demand', sprintf('b%d:b%d', range(1), range(2)))';
 gp_cap = xlsread(xlsx_file, 'GP_Flow', sprintf('e%d:e%d', range(1), range(2)));
 hov_cap = xlsread(xlsx_file, 'HOV_Flow', sprintf('e%d:e%d', range(1), range(2)));
 
@@ -40,6 +40,12 @@ for i = 1:sz
   end
 end
 
+SR = [];
+for i = 1:277
+  sr = estimate_sr(mean(ORD(:, i:i+11)'), mean(FRD(:, i:i+11)'), max_sr);
+  SR = [SR sr'];
+end
+SR = [SR repmat(sr', 1, 11)];
 
 
-
+gen_sr_xml(gp_id, hov_id, or_id, fr_id, NDOFF(:, 1)', SR);
--- matlab/init_680N_2025.m
+++ matlab/init_680N_2025.m
@@ -2,12 +2,13 @@
 cfg_folder = fullfile(root,'config','680N_2025');
 
 xlsx_file        = fullfile(cfg_folder,'I680NB_Data_2025.xlsx');
+csv_file        = fullfile(cfg_folder,'node_offramps.csv');
 cfg_starter      = fullfile(cfg_folder,'680N.xml');
 sr_initial_guess = fullfile(cfg_folder,'680N_sr.xml');
 
 range = [2 149];  % 680 NB
 
-hov_prct = 0.15;
+hov_prct = 0.18;
 
 pm_dir = 1;
 
