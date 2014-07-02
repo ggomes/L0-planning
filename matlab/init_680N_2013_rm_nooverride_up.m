ea5586a3e2e2c3d4910eb32ab93e6519ab8e0387
Gabriel Gomes <gomes@path.berkeley.edu>
2014-07-01 18:18:24 -0700

    script_run_scenarios

--- matlab/init_680N_2013_rm_nooverride_up.m
+++ matlab/init_680N_2013_rm_nooverride_up.m
@@ -7,6 +7,8 @@ sr_initial_guess = fullfile(cfg_folder,'680N_sr.xml');
 rm_act_cntrl     = fullfile(cfg_folder,'rm_nooverride_up.xml');
 sr_act_cntrl     = fullfile(cfg_folder,'act_cntrl_sr_generator.xml');
 cfg_scenario     = fullfile(cfg_folder,'680_rm_nooverride_up.xml');
+beatsprop_scenario = fullfile(cfg_folder,'rm.properties');
+
 
 range = [2 149];  % 680 NB
 
--- matlab/script_run_scenarios.m
+++ matlab/script_run_scenarios.m
@@ -4,107 +4,132 @@ close all
 init_config;
 
 % 1. Load basic network ..................................................
-disp('1. Load basic network');
-tic
 ptr = BeatsSimulation;
 ptr.load_scenario(cfg_starter);
-disp(['Done in ' num2str(toc) ' seconds.']);
 
 % 2. Load or demands from xls ............................................
-disp('2. Load or demands from xls');
-tic
 ptr.scenario_ptr.scenario.DemandSet = attach_onramp_demands(xlsx_file,range,hov_prct);
-disp(['Done in ' num2str(toc) ' seconds.']);  
 
 % 3. Load initial guess at fr splits .....................................
-disp('3. Load initial guess at fr splits');
-tic
 ptr.scenario_ptr.scenario.SplitRatioSet = xml_read(sr_initial_guess);
-disp(['Done in ' num2str(toc) ' seconds.']);
 
 % 4. Run greedy policy ...................................................
-disp('4. Run greedy policy');
-tic
-ptr.scenario_ptr.save(cfg_gp);
-system(['java -jar ' beats_jar opt_minus_s beatsprop_gp]);
-ptr.simulation_done = true;
-ptr.load_simulation_output('../beats_output/gp');
-disp(['Done in ' num2str(toc) ' seconds.']);
+ptr.run_beats( struct( 'SCENARIO','..\\config\\680N_+RM_2013\\generated\\gp.xml',...
+                       'SIM_DT','5',...
+                       'OUTPUT_PREFIX','..\\beats_output\\gp',...
+                       'SPLIT_LOGGER_PREFIX','..\\beats_output\\sr',...
+                       'SPLIT_LOGGER_DT','300',...
+                       'OUTPUT_DT','5' ) );
+ptr.plot_performance
 
 % 5. aggregate GP/HOV split ratios to 5min ...............................
-disp('5. aggregate GP/HOV split ratios to 5min');
-tic
 is_5min_gp = ~isempty(strfind(beatsprop_gp,'beats_gp_5min.properties'));
 ptr.scenario_ptr.scenario.SplitRatioSet = compute_5min_splits_from_sim(ptr,gp_out,is_5min_gp);
-disp(['Done in ' num2str(toc) ' seconds.']);  
 
 % 6. Take fr demands from xls to xml .....................................
-disp('6. Take fr demands from xls to xml')
-tic
 write_offramp_demand_xml(xlsx_file,fr_demand_file,range)
-disp(['Done in ' num2str(toc) ' seconds.']);  
 
 % 7. Load split actuators/controllers .....................................
-disp('7. Load split actuators/controllers');
-tic
 act_and_ctrl = xml_read(sr_act_cntrl);
 act_and_ctrl.ControllerSet.controller.parameters.parameter.ATTRIBUTE.value = ...
     fullfile(cfg_gen_folder,'fr_demand.xml');
 ptr.scenario_ptr.scenario.ControllerSet = act_and_ctrl.ControllerSet;
 ptr.scenario_ptr.scenario.ActuatorSet = act_and_ctrl.ActuatorSet;
-ptr.scenario_ptr.save(cfg_srout);
-disp(['Done in ' num2str(toc) ' seconds.']);
 
 % 8. Run offramp split ratio computation .................................
-disp('8. Run offramp split ratio computation');
-tic
 ptr.reset_simulation;
-system(['java -jar ' beats_jar opt_minus_s beatsprop_sr_out]);
-ptr.simulation_done = true;
-ptr.load_simulation_output('../beats_output/srout');
-disp(['Done in ' num2str(toc) ' seconds.']);
+ptr.run_beats( struct( 'SCENARIO','..\\config\\680N_+RM_2013\\generated\\srout.xml',...
+                       'SIM_DT','5',...
+                       'OUTPUT_PREFIX','..\\beats_output\\srout',...
+                       'RUN_MODE','fw_fr_split_output',...
+                       'OUTPUT_DT','300',...
+                       'SPLIT_LOGGER_PREFIX','..\\beats_output\\sr' ) );
+ptr.plot_performance
 
-save aaa
-
-% 8.5 Load and save offramp split ratios to xml (used for rm scenarios) ..
+% 8 Load and save offramp split ratios to xml (used for rm scenarios) ..
 link_id_begin_end = ptr.scenario_ptr.link_id_begin_end;
 link_ids = ptr.scenario_ptr.get_link_ids;
 link_types = ptr.scenario_ptr.get_link_types;
 fr_links_ids = link_ids(strcmp(link_types,'Off-Ramp'));
 srps = [];
 for i=1:length(fr_links_ids)
-    
+        
     begin_node = link_id_begin_end(link_id_begin_end(:,1)==fr_links_ids(i),2);
-    srp = ptr.scenario_ptr.get_split_ratios_for_node_id(begin_node);
+    
+    % load computed fr split
+    comp_sr = load(fullfile(root,'beats_output',sprintf('sr%d.txt',begin_node)));
+    
+    % keep only splits going to offramp
+    comp_sr = comp_sr(comp_sr(:,3)==fr_links_ids(i),:);
+    
+    % get existing split ratio profile
+    [srp,srp_ind] = ptr.scenario_ptr.get_split_ratios_for_node_id(begin_node);
+    
+    % if we have it, extract splits and overwrite
     if(~isempty(srp))
 
         A = [srp.splitratio.ATTRIBUTE];
         
+        % get splits fwy/HOV -> offramp splits
         [~,x]=ismember([A.link_in],link_ids);
         ind = strcmp(link_types(x),'Freeway') | strcmp(link_types(x),'HOV');
         ind = ind & [A.link_out]==fr_links_ids(i);
+        ind = find(ind);
         
-        if(sum(ind)~=4)
-            error('I was expecting 4.')
+        if(length(ind)~=2 && length(ind)~=4)
+            error('I was expecting 2 or 4.')
         end
-        
-        xxx = srp.splitratio(ind);
-        
-        for j=1:length(xxx)
-            xxx(1).CONTENT
+                
+        for j=1:length(ind)
 
-            xxx(2).ATTRIBUTE
+            link_in = srp.splitratio(ind(j)).ATTRIBUTE.link_in;
+            veh_type = srp.splitratio(ind(j)).ATTRIBUTE.vehicle_type_id;
+            
+            % computed split ratio
+            new_fr_split = comp_sr(comp_sr(:,2)==link_in & comp_sr(:,4)==veh_type,5);
         
+            % aggregate to 5 min
+            new_fr_split = mean(reshape(new_fr_split(2:end),60,288),1);
+            
+            % replace old fr split
+            ptr.scenario_ptr.scenario.SplitRatioSet.splitRatioProfile(srp_ind).splitratio(ind(j)).CONTENT = new_fr_split;
+            
         end
-        
         
     end
 end
 
+% remove actuators/controllers
+ptr.scenario_ptr.scenario = rmfield(ptr.scenario_ptr.scenario,{'ControllerSet','ActuatorSet'});
 
-% 9. Put the result into Excel spreadsheet ...............................
-disp('9. Put the result into Excel spreadsheet')
-tic
-[GP_V, GP_F, GP_D, HOV_V, HOV_F, HOV_D, ORD, ORF, FRD, FRF, ORQ] = extract_simulation_data(ptr,xlsx_file,range);
-disp(['Done in ' num2str(toc) ' seconds.']); 
+% NEED TO CHECK THAT THIS RUN GIVES SIMILAR RESULT TO FR DEMAND CONTROLLER RUN
+ptr.reset_simulation;
+ptr.run_beats( struct( 'SCENARIO','..\\config\\680N_+RM_2013\\generated\\srout_2.xml',...
+                       'SIM_DT','5',...
+                       'OUTPUT_PREFIX','..\\beats_output\\srout_2',...
+                       'OUTPUT_DT','300',...
+                       'SPLIT_LOGGER_PREFIX','..\\beats_output\\sr' ) );
+ptr.plot_performance
+
+% % 9. Put the result into Excel spreadsheet ...............................
+% disp('9. Put the result into Excel spreadsheet')
+% tic
+% [GP_V, GP_F, GP_D, HOV_V, HOV_F, HOV_D, ORD, ORF, FRD, FRF, ORQ] = extract_simulation_data(ptr,xlsx_file,range);
+% disp(['Done in ' num2str(toc) ' seconds.']); 
+
+% Attach ramp metering actuators and controllers .........................
+act_and_ctrl = xml_read(rm_act_cntrl);
+ptr.scenario_ptr.scenario.ControllerSet = act_and_ctrl.ControllerSet;
+ptr.scenario_ptr.scenario.ActuatorSet = act_and_ctrl.ActuatorSet;
+ptr.scenario_ptr.scenario.SensorSet = act_and_ctrl.SensorSet;
+
+% run scenario
+ptr.reset_simulation;
+ptr.run_beats( struct( 'SCENARIO','..\\config\\680N_+RM_2013\\680_rm_nooverride_up.xml',...
+                       'SIM_DT','5',...
+                       'OUTPUT_PREFIX','..\\beats_output\\680_rm_nooverride_up',...
+                       'OUTPUT_DT','300') );
+ptr.plot_performance
+
+
 
