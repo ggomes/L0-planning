ef2259d07bd97cd911115f62586d673ccf13debb
Alex <akurzhan@gmail.com>
2014-09-16 16:51:44 -0700

    680S base 2013 init cleanup

--- matlab/init_680S_2013.m
+++ matlab/init_680S_2013.m
@@ -1,8 +1,9 @@
+% 680S 2013 baseline init file
 
 cfg_folder = fullfile(root,'config','680S');
 
 xlsx_file        = fullfile(cfg_folder,'I680SB_Data.xlsx');
-cfg_starter      = fullfile(cfg_folder,'680S.xml');
+cfg_starter      = fullfile(cfg_folder,'680S_generated.xml');
 sr_initial_guess = fullfile(cfg_folder,'680S_sr.xml');
 
 range = [2 147];  % 680 SB
