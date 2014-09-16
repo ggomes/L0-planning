b2610a8e07dc8b7eee49ab4b30387e1229b05c16
Alex <akurzhan@gmail.com>
2014-09-16 16:12:41 -0700

    680S Scenario 1_0 init 2013

--- matlab/init_680S_2013_S1_0.m
+++ matlab/init_680S_2013_S1_0.m
@@ -1,13 +1,11 @@
+% 680S Scenario 1 2013: Aux lane only
 
 cfg_folder = fullfile(root,'config','680S_Scenario_1_0');
 
 xlsx_file        = fullfile(cfg_folder,'I680SB_+Aux.xlsx');
-cfg_starter      = fullfile(cfg_folder,'680S.xml');
-sr_initial_guess = fullfile(cfg_folder,'680S_sr.xml');
+cfg_starter      = fullfile(cfg_folder,'680S_generated.xml');
 
 range = [2 147];  % 680 SB
-
-hov_prct = 0.135;
 
 pm_dir = -1;
 
