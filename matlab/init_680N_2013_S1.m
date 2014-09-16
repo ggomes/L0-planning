f62abf0e34e2666b2da9267517c066c980996758
Alex <akurzhan@gmail.com>
2014-09-15 18:26:56 -0700

    clean initialization for 680 Scenario 1

--- matlab/init_680N_2013_S1.m
+++ matlab/init_680N_2013_S1.m
@@ -2,13 +2,9 @@
 cfg_folder       = fullfile(root,'config','680N_S1_2013');
 
 xlsx_file        = fullfile(cfg_folder,'I680NB_S1.xlsx');
-csv_file         = fullfile(cfg_folder,'node_offramps.csv');
 cfg_starter      = fullfile(cfg_folder,'680N_generated.xml');
-sr_initial_guess = fullfile(cfg_folder,'680N_sr.xml');
-sr_init_file     = fullfile(cfg_folder,'680N_sr_init.xml');
 
 range = [2 149];  % 680 NB
 
 pm_dir = 1;
 
-rm_control = 0;
--- matlab/init_680N_2013_S1_0.m
+++ matlab/init_680N_2013_S1_0.m
@@ -1,13 +1,10 @@
 
 cfg_folder = fullfile(root,'config','680N_Scenario_1_0');
 
-xlsx_file        = fullfile(cfg_folder,'I680NB_+Aux.xlsx');
-cfg_starter      = fullfile(cfg_folder,'680N.xml');
-sr_initial_guess = fullfile(cfg_folder,'680N_sr.xml');
+xlsx_file        = fullfile(cfg_folder,'I680NB_S1_AuxLane.xlsx');
+cfg_starter      = fullfile(cfg_folder,'680N_generated.xml');
 
 range = [2 149];  % 680 NB
-
-hov_prct = 0.15;
 
 pm_dir = 1;
 
