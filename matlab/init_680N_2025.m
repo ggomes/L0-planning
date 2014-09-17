edf3361911d3422f994c890419d2f8a049438d23
Alex <akurzhan@gmail.com>
2014-09-16 20:02:04 -0700

    680N 2025 baseline init cleanup

--- matlab/init_680N_2025.m
+++ matlab/init_680N_2025.m
@@ -1,14 +1,13 @@
+% 680N 2025 baseline init
 
 cfg_folder = fullfile(root,'config','680N_2025');
 
 xlsx_file        = fullfile(cfg_folder,'I680NB_Data_2025.xlsx');
-csv_file        = fullfile(cfg_folder,'node_offramps.csv');
-cfg_starter      = fullfile(cfg_folder,'680N.xml');
-sr_initial_guess = fullfile(cfg_folder,'680N_sr.xml');
+cfg_starter      = fullfile(cfg_folder,'680N_generated.xml');
 
 range = [2 149];  % 680 NB
 
-hov_prct = 0.18;
-
 pm_dir = 1;
+
+no_ml_queue = 0;
 
