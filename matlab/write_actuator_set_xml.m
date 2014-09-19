aea6b87fbbafa74e1fe37d8de198c68d946c1645
Alex <akurzhan@gmail.com>
2014-09-18 21:02:07 -0700

    Writing actuators

--- matlab/write_actuator_set_xml.m
+++ matlab/write_actuator_set_xml.m
@@ -7,6 +7,10 @@ function write_actuator_set_xml(fid, xlsx_file, range, or_id)
 disp('  F. Generating actuator set...');
 
 
+min_rate = 240;
+max_rate = 900;
+
+
 % On-ramp parameters
 lanes = xlsread(xlsx_file, 'Configuration', sprintf('p%d:p%d', range(1), range(2)))';
 metered = xlsread(xlsx_file, 'Configuration', sprintf('q%d:q%d', range(1), range(2)))';
@@ -16,7 +20,23 @@ sz = range(2) - range(1) + 1;
 
 fprintf(fid, ' <ActuatorSet id="1" project_id="1">\n');
 for i = 1:sz
-	;
+  if (or_id(i) ~= 0) & (metered(i) ~= 0)
+    ml = round(metered(i) / lanes(i));
+    mq = 100000;
+    fprintf(fid, '  <actuator id="%d">\n', or_id(i));
+    fprintf(fid, '   <scenarioElement id="%d" type="link"/>\n', or_id(i));
+    fprintf(fid, '   <actuator_type id="0" name="ramp_meter"/>\n');
+    if qlimits(i) ~= 0
+      fprintf(fid, '   <queue_override strategy="max_rate"/>\n');
+      mq = qlimits(i);
+    end
+    fprintf(fid, '   <parameters>\n');
+    fprintf(fid, '    <parameter name="min_rate_in_vphpl" value="%d">\n', min_rate*ml);
+    fprintf(fid, '    <parameter name="max_rate_in_vphpl" value="%d">\n', max_rate*ml);
+    fprintf(fid, '    <parameter name="max_queue_vehicles" value="%d">\n', mq);
+    fprintf(fid, '   </parameters>\n');
+    fprintf(fid, '  </actuator>\n');
+  end
 end
 
 
