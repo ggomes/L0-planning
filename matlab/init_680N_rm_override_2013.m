9216370c8bcd43fbff0971e5376a9573a3d818db
Alex <akurzhan@gmail.com>
2014-09-18 18:46:35 -0700

    init files for 680 2013 with RM

---
+++ matlab/init_680N_rm_override_2013.m
@@ -0,0 +1,11 @@
+% 680N base year 2013 data with ramp metering and queue control
+
+cfg_folder = fullfile(root,'config','680N_+RM_2013');
+
+xlsx_file        = fullfile(cfg_folder,'I680NB_RM_override.xlsx');
+cfg_starter      = fullfile(cfg_folder,'680N_generated.xml');
+
+range = [2 149];  % 680 NB
+
+pm_dir = 1;
+
--- matlab/init_680S_rm_override_2013.m
+++ matlab/init_680S_rm_override_2013.m
@@ -1,8 +1,8 @@
 
 cfg_folder = fullfile(root,'config','680S_+RM_2013');
 
-xlsx_file        = fullfile(cfg_folder,'I680SB_RM_nooverride_up.xlsx');
-cfg_starter      = fullfile(cfg_folder,'680S_rm_nooverride_up.xml');
+xlsx_file        = fullfile(cfg_folder,'I680SB_RM_nooverride_dn.xlsx');
+cfg_starter      = fullfile(cfg_folder,'680S_rm_nooverride_dn.xml');
 sr_initial_guess = fullfile(cfg_folder,'680S_sr.xml');
 
 range = [2 147];  % 680 SB
